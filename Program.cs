2846c903a02fa4c8ccc4f217d69d9a50db5b2662
Suzanna Buniatyan <s.buniatyan09055@icloud.com>
2024-02-06 23:28:09 +0300

    zadanie1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("введите значение n:");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("введите значение m:");
+int m = int.Parse(Console.ReadLine());
+
+int result = AckermannFunction(m, n);
+
+Console.WriteLine($"результат функции для {m} и {n}");
+
+static int AckermannFunction(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return AckermannFunction(m - 1, 1);
+    else if (m > 0 && n > 0)
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    return 0;
+}
